5a83ac1bd2195667bc7077f93e0ff4404cfc6549
Ervin Barta <ervin@devbox.(none)>
2013-12-16 18:12:39 +0100

    Remove unneeded files

--- features/step_definitions/record_removal_steps.rb
+++
@@ -1,9 +0,0 @@
-Then(/^I should be able to remove items from the poll$/) do
-  step "I should be able to add items to the poll"
-
-  click_link "Edit"
-  click_link "Remove"
-  click_button "Update"
-
-  page.should_not have_content("My Record")
-end
