2db37f994c8238d9a8c0f2441b2adefc031297c2
Ervin Barta <ervin@devbox.(none)>
2013-12-19 12:55:12 +0100

    Change the lock's link method to POST

--- features/step_definitions/poll_lockdown_steps.rb
+++ features/step_definitions/poll_lockdown_steps.rb
@@ -1,5 +1,5 @@
 Then(/^I should be able to lock it down$/) do
-  click_link "Show"
-  click_button "Lock poll"
+  click_link "Edit"
+  click_link "Lock poll"
   page.should have_content("Poll locked!")
 end
