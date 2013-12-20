71f5be63b363a8ccd884188f570360e4a9745686
Ervin Barta <ervin@devbox.(none)>
2013-12-20 10:56:31 +0100

    Added email address to the top bar

---
+++ features/step_definitions/poll_invitation_steps.rb
@@ -0,0 +1,7 @@
+When(/^I lock down the poll$/) do
+  step 'I should be able to lock it down'
+end
+
+Then(/^I should be able to invite others$/) do
+  pending # express the regexp above with the code you wish you had
+end
