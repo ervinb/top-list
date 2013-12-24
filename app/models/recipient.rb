8b39b4f7c39f1cae1e4bed8832aaa61f070d8adc
Ervin Barta <ervin@devbox.(none)>
2013-12-24 17:44:16 +0100

    Add a method to the Recipient model to check if she voted on a given poll

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -2,4 +2,10 @@ class Recipient < ActiveRecord::Base
   belongs_to :poll
 
   validates :email, :presence => true
+
+  def has_voted(poll_id)
+    poll = Poll.find(poll_id)  
+    poll.scores.where(:token => token).exists? 
+  end
+    
 end
--- spec/models/recipient_spec.rb
+++ spec/models/recipient_spec.rb
@@ -8,4 +8,20 @@ describe Recipient do
   it { should have_db_column(:updated_at).of_type(:datetime) }
 
   it { should belong_to(:poll) }
+
+  describe ".has_voted" do
+
+    before :each do
+      @poll = FactoryGirl.create(:poll)
+      @entry = FactoryGirl.create(:entry, :poll => @poll)
+      @recipient = FactoryGirl.create(:recipient)
+    end
+
+    it "returns true if the recipient has voted" do
+      @poll.build_scores([@entry.id], @recipient.token, nil)
+      @recipient.has_voted(@poll.id).should be_true
+    end
+        
+  end
+
 end
